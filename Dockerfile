ca3895806a8a81672582ad7238d118212f70878f
Redamancy2319 <101871681+Redamancy2319@users.noreply.github.com>
2023-08-29 02:56:46 +0800

    refactor:Replacing supervisor with tini to enhance process management capabilities

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,21 @@
-FROM alpine:latest
+FROM debian:11-slim
 
 ARG NEZHA_VER=0.15.4
 
-ENV domain=localhost port=5555 secret=password args=--disable-command-execute\ --disable-auto-update platform=Linux version=
+ENV domain="" port="5555" secret="" args="--disable-auto-update" platform="" version=""
 
 WORKDIR /usr/local/bin
 
 COPY ./entrypoint.sh /usr/local/bin/
 
-RUN apk update --no-cache &&\
-    apk add --no-cache supervisor &&\
-    wget -O ./nezha-agent.zip "https://github.com/nezhahq/agent/releases/download/v${NEZHA_VER}/nezha-agent_linux_$(uname -m | sed "s#x86_64#amd64#; s#aarch64#arm64#").zip" &&\
+RUN apt-get update &&\
+    apt-get install -y --no-install-recommends tini wget unzip ca-certificates &&\
+    rm -rf /var/lib/apt/lists/* &&\
+    arch=$(uname -m | sed "s#x86_64#amd64#; s#aarch64#arm64#; s#i386#386#") &&\
+    wget -O ./nezha-agent.zip -t 4 -T 5 "https://github.com/nezhahq/agent/releases/download/v${NEZHA_VER}/nezha-agent_linux_${arch}.zip" &&\
     unzip ./nezha-agent.zip &&\
     rm -f ./nezha-agent.zip &&\
-    rm -f /etc/alpine-release &&\
     chmod +x ./entrypoint.sh &&\
     chmod +x ./nezha-agent
 
-ENTRYPOINT ["sh","./entrypoint.sh"]
+ENTRYPOINT ["/usr/bin/tini","-g","--","/usr/local/bin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
-#supervisor config
-cat > /etc/supervisord.conf << EOF
-[supervisord]
-nodaemon=true
-logfile=/var/log/supervisord.log
-[program:agent]
-command=nezha-agent -s ${domain}:${port} -p ${secret} ${args}
-autostart=true
-autorestart=true
-EOF
 
-#modify platform
-if [ -n "${platform}" ] && [ -n "${version}" ]; then
-    sed -i "s/^ID=.*/ID=${platform}/" /etc/os-release
-    sed -i "s/^VERSION_ID=.*/VERSION=${version}/" /etc/os-release
-else
-    platform=$(uname -v)
-    version=$(uname -r)
-    if echo "$platform" | grep -qi "debian"; then
-        platform="debian"
-    elif echo "$platform" | grep -qi "ubuntu"; then
-        platform="ubuntu"
-    else
-        platform="Linux"
-    fi
-    version=${version%%-*}
-    sed -i "s/^ID=.*/ID=${platform}/" /etc/os-release
-    sed -i "s/^VERSION_ID=.*/VERSION=${version}/" /etc/os-release
+#check domain and secret variables
+if [[ -z "${domain}" || -z "${secret}" ]]; then
+    echo "Domain and secret cannot be empty!"
+    exit 1
 fi
 
+#modify platform
+if [ -n "${platform}" ] || [ -n "${version}" ]; then
+    rm -f /etc/debian_version
+    if [ -n "${platform}" ]; then
+        sed -i "s/^ID=.*/ID=${platform}/" /etc/os-release
+    fi
 
-#start supervisor
-supervisord -c /etc/supervisord.conf
+    if [ -n "${version}" ]; then
+        sed -i "s/^VERSION_ID=.*/VERSION_ID=${version}/" /etc/os-release
+    fi
+fi
+
+exec /usr/local/bin/nezha-agent -s ${domain}:${port} -p ${secret} ${args}
